59c8db860bc989c3cac7005dcdee5d3dc5d3e972
Layne <saxalayne@gmail.com>
2023-08-16 13:01:39 -0600

    add db

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from models import db,  Card, Binder
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
