c35a3d47a5dea3e757fa94b9cebfa9765745a965
Layne <saxalayne@gmail.com>
2023-08-16 11:34:44 -0600

    added boilerplate

--- server/app.py
+++ server/app.py
@@ -1,7 +1,11 @@
 # Routes defined in app.py
 from flask import Flask
+from flask_migrate import Migrate
+from models import db,  Card, Binder
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 @app.route("/")
 def hello_world():
--- server/models.py
+++ server/models.py
@@ -1,3 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    set = db.Column(db.String)
+    image = db.Column(db.String)
+
+class Binder(db.Model):
+    __tablename__ = 'binders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
